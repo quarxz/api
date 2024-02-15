9a8083a649aa4cad6f023853900ea15e79822af2
Falk König <fk@falkkoenig.de>
2024-02-15 11:13:19 +0100

    add select function

--- src/Components/OpenStreetMap.jsx
+++ src/Components/OpenStreetMap.jsx
@@ -1,22 +1,112 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState, useEffect, useRef } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvent,
+  useMapEvents,
+  useMap,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 import styles from "./OpenStreetMap.module.css";
 
-export function OpenStreetMap() {
+function SetViewOnClick({ animateRef }) {
+  const map = useMapEvent("click", (e) => {
+    map.setView(e.latlng, map.getZoom(), {
+      animate: animateRef.current || false,
+    });
+  });
+  return null;
+}
+
+function LocationMarker({ coordinates }) {
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click() {
+      map.locate();
+      console.log(map);
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
+
+function ChangeView({ center, zoom }) {
+  const map = useMap();
+  console.log(center);
+  map.flyTo(center, zoom);
+  return null;
+}
+
+export function OpenStreetMap({ coordinates, users, isloading }) {
+  const [latLong, setLatLong] = useState([51.505, -0.09]);
+
   const position = [51.505, -0.09];
+  const animateRef = useRef(false);
+
   return (
     <>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+      <p>
+        <label>
+          <input
+            type="checkbox"
+            onChange={() => {
+              animateRef.current = !animateRef.current;
+            }}
+          />
+          Animate panning
+        </label>
+      </p>
+      <div>
+        {isloading ? (
+          <p>Lade Daten...</p>
+        ) : (
+          <select
+            onChange={(e) => {
+              let x = e.target.value.split(",");
+              setLatLong([x[0], x[1]]);
+            }}
+          >
+            {users.map((user) => {
+              return (
+                <option
+                  key={user.cell}
+                  value={[
+                    user.location.coordinates.latitude,
+                    user.location.coordinates.longitude,
+                  ]}
+                >
+                  {user.name.first} {user.name.last}
+                </option>
+              );
+            })}
+          </select>
+        )}
+      </div>
+      <MapContainer center={latLong} zoom={13} scrollWheelZoom={true}>
+        <ChangeView center={latLong} zoom={13} />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={latLong}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
+        <SetViewOnClick animateRef={animateRef} />
+        <LocationMarker />
       </MapContainer>
     </>
   );
